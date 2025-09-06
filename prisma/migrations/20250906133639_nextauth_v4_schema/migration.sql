c0ed221c1fcb52b3f26bcf1da60eb07d8ebb6b88
extern.omer.mizrahi <extern.omer.mizrahi@cymotive.com>
2025-09-06 16:40:50 +0300

    auth with prisma and next-auth

---
+++ prisma/migrations/20250906133639_nextauth_v4_schema/migration.sql
@@ -0,0 +1,84 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpires` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerId` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerType` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessToken` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `VerificationRequest` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[provider,providerAccountId]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `provider` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `Account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropIndex
+DROP INDEX "public"."Account_providerId_providerAccountId_key";
+
+-- DropIndex
+DROP INDEX "public"."Session_accessToken_key";
+
+-- AlterTable
+ALTER TABLE "public"."Account" DROP COLUMN "accessToken",
+DROP COLUMN "accessTokenExpires",
+DROP COLUMN "createdAt",
+DROP COLUMN "providerId",
+DROP COLUMN "providerType",
+DROP COLUMN "refreshToken",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "access_token" TEXT,
+ADD COLUMN     "expires_at" INTEGER,
+ADD COLUMN     "id_token" TEXT,
+ADD COLUMN     "provider" TEXT NOT NULL,
+ADD COLUMN     "refresh_token" TEXT,
+ADD COLUMN     "scope" TEXT,
+ADD COLUMN     "session_state" TEXT,
+ADD COLUMN     "token_type" TEXT,
+ADD COLUMN     "type" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "public"."Session" DROP COLUMN "accessToken",
+DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt";
+
+-- AlterTable
+ALTER TABLE "public"."User" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt";
+
+-- DropTable
+DROP TABLE "public"."VerificationRequest";
+
+-- CreateTable
+CREATE TABLE "public"."VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "public"."VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "public"."VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "public"."Account"("provider", "providerAccountId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
