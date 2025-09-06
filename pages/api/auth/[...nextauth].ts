6cce1abc9d8ff048c72df55e7a4693c3aa72b950
extern.omer.mizrahi <extern.omer.mizrahi@cymotive.com>
2025-09-06 19:26:16 +0300

    create stripe customer and updaet the user model by prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
+import { id } from './../../../node_modules/effect/src/Fiber';
+import { version } from './../../../node_modules/@types/react-dom/index.d';
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import Stripe from "stripe"
 
 const prisma = new PrismaClient()
 
@@ -13,4 +16,22 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
+    events: {
+        createUser: async ({ user }) => {
+            // get stripe access
+            const stripe = new Stripe(process.env.STRIP_SECRET_KEY!)
+            // create stripe customer
+            if (user.name && user.email) {
+                const customer = await stripe.customers.create({
+                    name: user.name,
+                    email: user.email
+                })
+                // update prisma user model with stripe customer id
+                await prisma.user.update({
+                    where: { id: user.id },
+                    data: { stripeCustomerId: customer.id }
+                })
+            }
+        }
+    }
 })
